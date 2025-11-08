b392080c471eb6ca7bad2f9a13ed38c236a6aba9
Nthabiseng Molekwa <molekwanthabiseng@gmail.com>
2025-11-08 12:03:12 +0200

    Create webpack.config.js

---
+++ 0x04-TypeScript/task_1/webpack.config.js
@@ -0,0 +1,17 @@
+{
+  "name": "typescript-teacher-interface",
+  "version": "1.0.0",
+  "description": "Task 1 - TypeScript Teacher Interface",
+  "main": "dist/bundle.js",
+  "scripts": {
+    "build": "webpack",
+    "start": "webpack serve --open"
+  },
+  "devDependencies": {
+    "typescript": "^5.3.3",
+    "ts-loader": "^9.4.4",
+    "webpack": "^5.91.0",
+    "webpack-cli": "^5.1.4",
+    "webpack-dev-server": "^5.0.4"
+  }
+}
