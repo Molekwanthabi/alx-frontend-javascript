a77d8001f2c43a7dbabbfed8be39daa552ca87f8
Nthabiseng Molekwa <molekwanthabiseng@gmail.com>
2025-11-08 12:09:45 +0200

    Add director1 object and log to console
    Added a director object with properties and logged it.

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,3 +16,20 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+// should print
+// Object
+// firstName: "John"
+// fullTimeEmployee: true
+// lastName: "Doe"
+// location: "London"
+// numberOfReports: 17
